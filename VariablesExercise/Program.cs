4e5162f32281ebd11ab63d806ea1dc5302cf74e8
Mariam Khamisi <mariam5100@yahoo.com>
2023-12-08 00:14:16 +0300

    first update

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Swabby";
+            int dogAge = 14;
+            char initial = 'M';
+            bool playful = true;
+            double dbNum = 5.33;
+            decimal dcNum = 6.5m;
+
+            Console.WriteLine($"My dog {dogName} {initial}, was {dogAge} years old when he died");
+            Console.WriteLine($"It is {playful} that he was a playful dog in as much as he looked fierce");
+            Console.WriteLine($"Double value is {dbNum} whereas decimal value is {dcNum})");
         }
     }
 }
